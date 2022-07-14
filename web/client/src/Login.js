cdf3cdcc7822739a295bafd79c53303fd32ba08e
k.ochi <ochi@brave-heart.co.jp>
2022-07-14 18:53:05 +0900

    add tailwind

---
+++ web/client/src/Login.js
@@ -0,0 +1,11 @@
+function Login(){
+    return (
+        <form className="w-400 px-40 py-10 absolute text-center top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 bg-black">
+            <div>
+                <h1 className="text-white font-medium">LOGIN</h1>
+            </div>
+        </form>
+    )
+}
+
+export default Login;
--- web/server/index.js
+++ web/server/index.js
@@ -3,22 +3,30 @@ const web = express()
 const request = require('request')
 const port = process.env.PORT || 8080
 const urlPrefix = 'http://app:1323'
+const callback = (req, res) => {
+    const options = {
+        method: req.method,
+        url: urlPrefix + req.originalUrl,
+    };
+    console.log('method:' + options['method'])
+    console.log('url:' + options['url'])
+    console.log('originalUrl:' + req.originalUrl)
+    console.log('protocol:' + req.protocol)
+    console.log('parmas:' + req.params)
+    console.log('body:' + req.body)
+    request(options, function(err, response, body) {
+        res.send(body);
+    })
+}
+
+web.use(express.json())
 
 web.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-web.get(/.+/, (req, res) => {
-    const options = {
-        method: req.method,
-        url: urlPrefix + req.path,
-    };
-    console.log(options['method'])
-    console.log(options['url'])
-    request(options, function(err, response, body) {
-        res.send(body);
-    })
-})
+web.get(/.+/, callback);
+web.post(/.+/, callback);
 
 web.listen(port, () => {
     console.log(`listening on *:${port}`);
